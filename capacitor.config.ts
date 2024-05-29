cc3de39652697bfcf7d98ff6dae82463d27f4811
geegeekoby <uniqu3nam3@gmail.com>
2024-05-29 05:25:58 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MOVIES2NIGHT',
+  webDir: 'www'
+};
+
+export default config;
